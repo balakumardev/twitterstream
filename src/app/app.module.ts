200e1c24f77b864a873510995ddf7bd3a169a5a0
Bala Kumar <balakumarrulez@gmail.com>
2017-07-04 20:54:57 +0530

    Initial Commit. Just a prototype

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecentTweetsComponent } from './recent-tweets/recent-tweets.component';
+import { HashtagOverviewComponent } from './hashtag-overview/hashtag-overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecentTweetsComponent,
+    HashtagOverviewComponent
   ],
   imports: [
     BrowserModule
